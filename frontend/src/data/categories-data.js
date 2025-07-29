c24e4d1621c82cf91bb92d0f54495229cb09cb46
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-07-29 23:23:55 +0700

    separate data products and categories from products page

---
+++ frontend/src/data/categories-data.js
@@ -0,0 +1,24 @@
+const categories = [
+  {
+    label: "Motherboard",
+    _id: "111",
+  },
+  {
+    label: "CPU",
+    _id: "222",
+  },
+  {
+    label: "GPU",
+    _id: "333",
+  },
+  {
+    label: "RAM",
+    _id: "444",
+  },
+  {
+    label: "PSU",
+    _id: "555",
+  },
+]
+
+export default categories
