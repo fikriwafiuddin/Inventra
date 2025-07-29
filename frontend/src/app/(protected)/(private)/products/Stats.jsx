5824a221a25ddb9a595b14409f84f24d76c9f9fb
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-07-29 23:28:02 +0700

    style: change style stats component in products page

--- frontend/src/app/(protected)/(private)/products/Stats.jsx
+++ frontend/src/app/(protected)/(private)/products/Stats.jsx
@@ -4,15 +4,11 @@ import { InboxIcon, TriangleAlertIcon } from "lucide-react"
 function Stats() {
   return (
     <div className="grid grid-cols-2 gap-4">
-      <StatCard
-        title="Total Products"
-        value={100}
-        icon={<InboxIcon className="size-4" />}
-      />
+      <StatCard title="Total Products" value={100} icon={<InboxIcon />} />
       <StatCard
         title="Out of Stock"
         value={50}
-        icon={<TriangleAlertIcon className="size-4" />}
+        icon={<TriangleAlertIcon className="text-warning" />}
       />
     </div>
   )
