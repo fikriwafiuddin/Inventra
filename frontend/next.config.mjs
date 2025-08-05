8e3f14712f82b9e4e26bad872b3230bb68697f86
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-05 07:09:19 +0700

    feat: allowing cloudinary

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      new URL(
+        "https://res.cloudinary.com/dvsoqr5sn/image/upload/v1754348068/Inventra/**"
+      ),
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
