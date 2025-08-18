5f1e48d6ddc79cbe82987db0a64c8c3e3e02f9fc
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-18 21:20:59 +0700

    fix: fix logger mongodb

--- backend/src/utils/connectDB.js
+++ backend/src/utils/connectDB.js
@@ -2,8 +2,21 @@ import mongoose from "mongoose"
 import logger from "./logger.js"
 
 mongoose.set("debug", (collection, method, query, doc) => {
+  const safeStringify = (obj) => {
+    const cache = new Set()
+    return JSON.stringify(obj, (key, value) => {
+      if (typeof value === "object" && value !== null) {
+        if (cache.has(value)) {
+          return "[Circular]"
+        }
+        cache.add(value)
+      }
+      return value
+    })
+  }
+
   logger.info(
-    `[MONGO] ${collection}.${method} ${JSON.stringify(query)} ${JSON.stringify(
+    `[MONGO] ${collection}.${method} ${JSON.stringify(query)} ${safeStringify(
       doc
     )}`
   )
