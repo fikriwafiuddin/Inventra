f9a40deaff0403029a0d985ab05b1a7f61e0eaad
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-19 13:28:35 +0700

    style: change filter orders

--- frontend/src/app/(protected)/(private)/orders/page.jsx
+++ frontend/src/app/(protected)/(private)/orders/page.jsx
@@ -1,9 +1,20 @@
+"use client"
+
 import { Input } from "@/components/ui/input"
 import { SearchIcon } from "lucide-react"
-import FilterDate from "./FilterDate"
 import { DataTable } from "@/components/DataTable"
 import columnOrders from "./columnOrder"
 import AppPagination from "@/components/AppPagination"
+import { ChevronDownIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import { Label } from "@/components/ui/label"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { useState } from "react"
 
 const orders = [
   {
@@ -57,6 +68,13 @@ const orders = [
 ]
 
 function OrdersPage() {
+  const [openStart, setOpenStart] = useState(false)
+  const [openEnd, setOpenEnd] = useState(false)
+  const now = new Date()
+  const firstDay = new Date(now.getFullYear(), now.getMonth(), 1)
+  const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0)
+  const [start, setStart] = useState(firstDay)
+  const [end, setEnd] = useState(lastDay)
   return (
     <div className="space-y-4">
       {/* FILTER */}
@@ -72,7 +90,70 @@ function OrdersPage() {
             placeholder="Search order by id"
           />
         </div>
-        <FilterDate />
+        <div className="flex gap-1">
+          <div className="flex">
+            <Label htmlFor="start" className="px-1">
+              From
+            </Label>
+            <Popover open={openStart} onOpenChange={setOpenStart}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  id="start"
+                  className="w-48 justify-between font-normal"
+                >
+                  {start ? start.toLocaleDateString() : "Select date"}
+                  <ChevronDownIcon />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-auto overflow-hidden p-0"
+                align="start"
+              >
+                <Calendar
+                  mode="single"
+                  selected={end}
+                  captionLayout="dropdown"
+                  onSelect={(date) => {
+                    setStart(date)
+                    setOpenStart(false)
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div className="flex">
+            <Label htmlFor="end" className="px-1">
+              To
+            </Label>
+            <Popover open={openEnd} onOpenChange={setOpenEnd}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  id="end"
+                  className="w-48 justify-between font-normal"
+                >
+                  {end ? end.toLocaleDateString() : "Select date"}
+                  <ChevronDownIcon />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-auto overflow-hidden p-0"
+                align="start"
+              >
+                <Calendar
+                  mode="single"
+                  selected={end}
+                  captionLayout="dropdown"
+                  onSelect={(date) => {
+                    setEnd(date)
+                    setOpenEnd(false)
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
       </div>
 
       <DataTable data={orders} columns={columnOrders} />
