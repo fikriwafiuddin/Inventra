ef1afa0c8d49fe1a71f844172cc87eca06413792
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-21 21:35:54 +0700

    style: change form adjustment

--- frontend/src/app/(protected)/(private)/stock/adjustment/page.jsx
+++ frontend/src/app/(protected)/(private)/stock/adjustment/page.jsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { Badge } from "@/components/ui/badge"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -17,9 +16,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Form } from "@/components/ui/form"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Select,
   SelectContent,
@@ -27,43 +32,37 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import { SearchIcon } from "lucide-react"
+import adjustmentValidation from "@/lib/validations/adjustment-validation"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useState } from "react"
-
-const products = [
-  {
-    _id: 13,
-    sku: "MOB-001",
-    name: "TUF GAMING Z690-PLUS WIFI D4",
-    image: "/products/TUF GAMING Z690-PLUS WIFI D4(1).jpg",
-    price: 1600000,
-    stock: 10,
-    sold: 90,
-  },
-  {
-    _id: 14,
-    sku: "GPU-001",
-    name: "GeForce RTX™ 3060 VENTUS 2X 12G OC",
-    image: "/products/GeForce RTX™ 3060 VENTUS 2X 12G OC(2).png",
-    price: 5000000000,
-    stock: 15,
-    sold: 85,
-  },
-  {
-    _id: 15,
-    sku: "RAM-001",
-    name: "Kingston FURY Impact DDR4",
-    image: "/products/Kingston FURY Impact DDR4(2).jpg",
-    price: 400000,
-    stock: 3,
-    sold: 73,
-  },
-]
+import { useForm } from "react-hook-form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import { cn } from "@/lib/utils"
+import { Check, ChevronsUpDownIcon, Loader2Icon } from "lucide-react"
+import { useSearchProducts } from "@/services/hooks/product-hook"
 
 function AdjustmentsPage() {
-  const [form, setForm] = useState({
-    product: null,
+  const form = useForm({
+    resolver: zodResolver(adjustmentValidation.add),
   })
+  const [selectedProduct, setSelectedProduct] = useState(null)
+  const [isCustom, setIsCustom] = useState(false)
+  const [type, setType] = useState("increase")
+  const [searchTermProduct, setSearchTermProduct] = useState("")
+  const { isPending, data: products } = useSearchProducts(searchTermProduct)
+
   return (
     <div className="space-y-4">
       <Breadcrumb>
@@ -77,7 +76,7 @@ function AdjustmentsPage() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <Form>
+      <Form {...form}>
         <form>
           <Card>
             <CardHeader>
@@ -85,83 +84,162 @@ function AdjustmentsPage() {
             </CardHeader>
             <CardContent>
               <div className="space-y-4">
-                <div className="space-y-2">
-                  <Label>Quantity</Label>
-                  <div className="flex gap-2">
-                    <Select defaultValue="increase">
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select type" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="increase">Increase +</SelectItem>
-                        <SelectItem value="decrease">Decrease -</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <Input type="number" />
-                  </div>
-                </div>
-                <div className="space-y-2">
-                  <Label>Reason</Label>
-                  <Select>
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Select reason" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="damaged">Damaged</SelectItem>
-                      <SelectItem value="lost">Lost</SelectItem>
-                      <SelectItem value="found">Found</SelectItem>
-                      <SelectItem value="correction">Corection</SelectItem>
-                      <SelectItem value="custom">Custom</SelectItem>
-                    </SelectContent>
-                  </Select>
-                </div>
-              </div>
+                <FormField
+                  control={form.control}
+                  name="quantity"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="space-y-2">
+                        <FormLabel>Quantity</FormLabel>
+                        <FormControl>
+                          <div className="flex gap-2">
+                            <Select onValueChange={setType} defaultValue={type}>
+                              <SelectTrigger>
+                                <SelectValue placeholder="Select type" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="increase">
+                                  Increase +
+                                </SelectItem>
+                                <SelectItem value="decrease">
+                                  Decrease -
+                                </SelectItem>
+                              </SelectContent>
+                            </Select>
 
-              <div className="space-y-2 mt-6">
-                <div className="flex gap-10">
-                  <Label>Product:</Label>
-                  <p className="text-sm">
-                    {form.product
-                      ? form.product?.name
-                      : "Haven't selected a product yet"}
-                  </p>
-                </div>
-                <div className="relative flex-1 h-max">
-                  <div className="absolute top-0 bottom-0 left-2 flex items-center">
-                    <SearchIcon className="size-4" />
-                  </div>
-                  <Input
-                    className="pl-8 w-full"
-                    type="search"
-                    placeholder="Search products by SKU or name"
-                  />
-                </div>
-              </div>
-
-              <div className="border rounded-lg max-h-48 overflow-y-auto mt-2">
-                {products.map((product) => (
-                  <button
-                    className="block w-full"
-                    key={product._id}
-                    onClick={() => setForm({ ...form, product })}
-                  >
-                    <div
-                      className={`p-3 cursor-pointer hover:bg-secondary flex justify-between items-center ${
-                        form.product && form.product._id === product._id
-                          ? "bg-secondary border-l-4 border-l-primary"
-                          : ""
-                      }`}
-                    >
-                      <div className="text-left">
-                        <p className="font-medium text-sm">{product.name}</p>
-                        <p className="text-xs text-muted-foreground">
-                          {product.sku}
-                        </p>
+                            <Input type="number" {...field} />
+                          </div>
+                        </FormControl>
+                        <FormMessage />
                       </div>
-                      <Badge variant="outline">Stok: {product.stock}</Badge>
-                    </div>
-                  </button>
-                ))}
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="reason"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="space-y-2">
+                        <FormLabel>Reason</FormLabel>
+                        <FormControl>
+                          <>
+                            <Select
+                              onValueChange={(value) => {
+                                if (value === "custom") {
+                                  setIsCustom(true)
+                                  field.onChange("")
+                                } else {
+                                  setIsCustom(false)
+                                  field.onChange(value)
+                                }
+                              }}
+                              defaultValue={isCustom ? "" : field.value}
+                            >
+                              <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Select reason" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="damaged">Damaged</SelectItem>
+                                <SelectItem value="lost">Lost</SelectItem>
+                                <SelectItem value="found">Found</SelectItem>
+                                <SelectItem value="correction">
+                                  Corection
+                                </SelectItem>
+                                <SelectItem value="custom">Custom</SelectItem>
+                              </SelectContent>
+                            </Select>
+                            {isCustom && (
+                              <Input
+                                placeholder="Enter your custom reason"
+                                {...field}
+                              />
+                            )}
+                          </>
+                        </FormControl>
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="product"
+                  render={({ field }) => (
+                    <FormItem>
+                      <div className="space-y-1">
+                        <FormLabel>Product</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant="outline"
+                                role="combobox"
+                                className={cn(
+                                  "justify-between w-full",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value
+                                  ? selectedProduct?.name
+                                  : "Select product"}
+                                <ChevronsUpDownIcon className="opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="p-0 w-[var(--radix-popover-trigger-width)]">
+                            <Command filter={() => 1}>
+                              <CommandInput
+                                placeholder="Search supplier..."
+                                className="h-9"
+                                onValueChange={(value) =>
+                                  setSearchTermProduct(value)
+                                }
+                              />
+                              <CommandList>
+                                {isPending && (
+                                  <div className="flex items-center justify-center py-2">
+                                    <Loader2Icon className="h-4 w-4 animate-spin" />
+                                  </div>
+                                )}
+                                {!isPending && products?.length === 0 && (
+                                  <CommandEmpty>
+                                    Tidak ada supplier yang ditemukan.
+                                  </CommandEmpty>
+                                )}
+                                <CommandGroup>
+                                  {products?.map((product) => (
+                                    <CommandItem
+                                      value={product._id}
+                                      key={product._id}
+                                      onSelect={() => {
+                                        field.onChange(product._id)
+                                        setSelectedProduct(product)
+                                      }}
+                                    >
+                                      <Check
+                                        className={cn(
+                                          "mr-2 h-4 w-4",
+                                          product._id === field.value
+                                            ? "opacity-100"
+                                            : "opacity-0"
+                                        )}
+                                      />
+                                      {product.name}
+                                    </CommandItem>
+                                  ))}
+                                </CommandGroup>
+                              </CommandList>
+                            </Command>
+                          </PopoverContent>
+                        </Popover>
+                        <FormMessage />
+                      </div>
+                    </FormItem>
+                  )}
+                />
               </div>
             </CardContent>
             <CardFooter>
