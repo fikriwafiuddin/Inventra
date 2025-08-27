2d19b1234b6fec95b8fdb1c5902f4d46138ce529
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-27 13:45:03 +0700

    style: change style layout

--- frontend/src/app/(protected)/(private)/layout.jsx
+++ frontend/src/app/(protected)/(private)/layout.jsx
@@ -1,17 +1,19 @@
 import { AppSidebar } from "@/components/AppSidebar"
 import Navbar from "@/components/Navbar"
 import Providers from "@/components/Providers"
-import { SidebarProvider } from "@/components/ui/sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import React from "react"
 
 function LayoutPrivate({ children }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full px-4 pb-4">
-        <Navbar />
-        <Providers>{children}</Providers>
-      </main>
+      <SidebarInset className="overflow-hidden">
+        <main className="flex-1 px-4 pb-4">
+          <Navbar />
+          <Providers>{children}</Providers>
+        </main>
+      </SidebarInset>
     </SidebarProvider>
   )
 }
