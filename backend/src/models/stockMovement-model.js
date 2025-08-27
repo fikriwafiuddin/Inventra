072958cd8d78592c80c0658eda2e60f81872aa33
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-27 13:52:13 +0700

    feat: add model stock movement

---
+++ backend/src/models/stockMovement-model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose"
+
+const stockMovementSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  product: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    sku: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  movementType: {
+    type: String,
+    required: true,
+    enum: [
+      "adjustment",
+      "customerReturn",
+      "supplierReturn",
+      "opname",
+      "sales",
+      "purchase",
+    ],
+  },
+  sourceId: {
+    type: String,
+    required: true,
+  },
+  initialStock: {
+    type: Number,
+    required: true,
+  },
+  finalStock: {
+    type: Number,
+    required: true,
+  },
+
+  reason: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: new Date(),
+  },
+})
+
+const StockMovement = mongoose.model("StockMovement", stockMovementSchema)
+export default StockMovement
