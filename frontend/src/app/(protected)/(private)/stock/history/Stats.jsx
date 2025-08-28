d24f005ddfd9baf75a3e4eb69767beaf275eefe4
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-28 16:05:17 +0700

    feat: implement stock movement summary statistics and loading state in Stats component

--- frontend/src/app/(protected)/(private)/stock/history/Stats.jsx
+++ frontend/src/app/(protected)/(private)/stock/history/Stats.jsx
@@ -1,4 +1,6 @@
 import StatCard from "@/components/StatCard"
+import { Skeleton } from "@/components/ui/skeleton"
+import { useGetstockMovementSummary } from "@/services/hooks/statistic-hook"
 import {
   ArrowDownIcon,
   ArrowUpIcon,
@@ -7,20 +9,41 @@ import {
 } from "lucide-react"
 
 function Stats() {
+  const { isPending, data: statistic } = useGetstockMovementSummary()
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
-      <StatCard title="Total Transactions" value={10} icon={<PackageIcon />} />
-      <StatCard
-        title="Stock In"
-        value={10}
-        icon={<ArrowUpIcon className="text-success" />}
-      />
-      <StatCard
-        title="Stock Out"
-        value={100}
-        icon={<ArrowDownIcon className="text-destructive" />}
-      />
-      <StatCard title="Net Change" value={100} icon={<TrendingUpIcon />} />
+      {isPending ? (
+        <>
+          <Skeleton className="h-[125px] rounded-xl" />
+          <Skeleton className="h-[125px] rounded-xl" />
+          <Skeleton className="h-[125px] rounded-xl" />
+          <Skeleton className="h-[125px] rounded-xl" />
+        </>
+      ) : (
+        <>
+          <StatCard
+            title="Total Transactions"
+            value={statistic?.totalTransactions || 0}
+            icon={<PackageIcon />}
+          />
+          <StatCard
+            title="Stock In"
+            value={statistic?.stockIn || 0}
+            icon={<ArrowUpIcon className="text-success" />}
+          />
+          <StatCard
+            title="Stock Out"
+            value={statistic?.stockOut || 0}
+            icon={<ArrowDownIcon className="text-destructive" />}
+          />
+          <StatCard
+            title="Net Change"
+            value={(statistic?.stockIn || 0) - (statistic?.stockOut || 0)}
+            icon={<TrendingUpIcon />}
+          />
+        </>
+      )}
     </div>
   )
 }
