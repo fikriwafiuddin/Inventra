ba82a06cb75a0b1aed2a92e811c28301fa50ee58
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-30 06:02:44 +0700

    feat: add timestamp in adjustment model

--- backend/src/models/adjustment-model.js
+++ backend/src/models/adjustment-model.js
@@ -1,29 +1,32 @@
 import mongoose from "mongoose"
 
-const adjustmenSchema = new mongoose.Schema({
-  user: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  reason: {
-    type: String,
-    required: true,
-  },
-  product: {
-    id: {
+const adjustmenSchema = new mongoose.Schema(
+  {
+    user: {
       type: String,
       required: true,
     },
-    name: {
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    reason: {
       type: String,
       required: true,
     },
+    product: {
+      id: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+    },
   },
-})
+  { timestamps: true }
+)
 
 const Adjustment = mongoose.model("Adjustment", adjustmenSchema)
 export default Adjustment
