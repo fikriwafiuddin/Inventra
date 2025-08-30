9ff331d4e752f439f99a6ec0667da373aba4d256
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-08-30 14:38:49 +0700

    feat: add detail refund component and add formatCurrency for totalRefund

--- frontend/src/app/(protected)/(private)/stock/customer-returns/columns.jsx
+++ frontend/src/app/(protected)/(private)/stock/customer-returns/columns.jsx
@@ -1,6 +1,15 @@
 import { Button } from "@/components/ui/button"
-import { formatDate } from "@/lib/formatters"
+import { formatCurrency, formatDate } from "@/lib/formatters"
 import { EyeIcon } from "lucide-react"
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  Dialog,
+  DialogTrigger,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog"
 
 const columns = [
   {
@@ -10,6 +19,7 @@ const columns = [
   {
     accessorKey: "totalRefund",
     header: "Total Refund",
+    cell: ({ row }) => formatCurrency(row.getValue("totalRefund")),
   },
   {
     accessorKey: "date",
@@ -18,11 +28,51 @@ const columns = [
   },
   {
     header: "Actions",
-    cell: ({ row }) => (
-      <Button>
-        <EyeIcon />
-      </Button>
-    ),
+    cell: ({ row }) => {
+      const items = row.original.items
+      return (
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <EyeIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>View Refund Details</DialogTitle>
+            </DialogHeader>
+            <div className="grid grid-cols-3">
+              <p>Order Id</p>
+              <p className="col-span-2">: {row.getValue("orderId")}</p>
+              <p>Total Refund</p>
+              <p className="col-span-2">
+                : {formatCurrency(row.getValue("totalRefund"))}
+              </p>
+              <p>Date</p>
+              <p className="col-span-2">: {formatDate(row.getValue("date"))}</p>
+            </div>
+            <div className="">
+              <p>Items:</p>
+              <ul>
+                {items.map((item) => (
+                  <li key={item.id} className="flex justify-between">
+                    <span>
+                      {item.name} {item.quantity}x
+                    </span>
+                    <span>{formatCurrency(item.price)}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button>Close</Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      )
+    },
   },
 ]
 
