ab899bf6359c35ddf718c46d75caf09357cf6889
fikriwafiuddin <fikriwafiuddin913@gmail.com>
2025-09-24 18:41:44 +0700

    feat: add internationalizations in add product page

--- frontend/src/app/[locale]/(protected)/(private)/products/add/page.jsx
+++ frontend/src/app/[locale]/(protected)/(private)/products/add/page.jsx
@@ -1,31 +1,3 @@
-"use client"
-
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { Textarea } from "@/components/ui/textarea"
-import { useState } from "react"
-import Image from "next/image"
-import { useGetAllCategories } from "@/services/hooks/category-hook"
-import { Loader2Icon } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import productValidation from "@/lib/validations/product-validation"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -34,190 +6,45 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-import { useAddProduct } from "@/services/hooks/product-hook"
-import { redirect } from "next/navigation"
+import FormAdd from "./FormAdd"
+import { useTranslations } from "next-intl"
 
 function AddPage() {
-  const form = useForm({
-    resolver: zodResolver(productValidation.addProduct),
-    defaultValues: {
-      name: "",
-      price: 0,
-      category: "",
-      minStock: 0,
-      description: "",
-      image: undefined,
-    },
-  })
-  const [previewImg, setPreviewImg] = useState(null)
-  const [image, setImage] = useState(undefined)
-  const { isPending, data: categories } = useGetAllCategories()
-  const { mutate: saveProduct, isPending: saving, isSuccess } = useAddProduct()
-
-  const onSubmit = (data) => {
-    const formData = new FormData()
-    for (const key in data) {
-      formData.append(key, data[key])
-    }
-    formData.set("image", image)
-    saveProduct(formData)
+  const tAddProductPage = useTranslations("AddProductPage")
+  const breadcrumbTranslations = {
+    products: tAddProductPage("breadcrumb.products"),
+    add: tAddProductPage("breadcrumb.add"),
   }
 
-  const handleFileChange = (e) => {
-    const file = e.target.files?.[0]
-    if (file) {
-      const imageUrl = URL.createObjectURL(file)
-      setPreviewImg(imageUrl)
-      form.setValue("image", file)
-      setImage(file)
-    }
+  const tFrom = useTranslations("FormProduct")
+  const formTranslations = {
+    labelName: tFrom("labelName"),
+    placeholderName: tFrom("placeholderName"),
+    labelPrice: tFrom("labelPrice"),
+    labelCategory: tFrom("labelCategory"),
+    placeholderCategory: tFrom("placeholderCategory"),
+    minStockLabel: tFrom("minStockLabel"),
+    descriptionLabel: tFrom("descriptionLabel"),
+    imageLabel: tFrom("imageLabel"),
+    saveButton: tFrom("saveButton"),
   }
-
-  if (isSuccess) {
-    redirect("/products")
-  }
-
   return (
     <div className="space-y-4">
       <Breadcrumb>
         <BreadcrumbList>
           <BreadcrumbItem>
-            <BreadcrumbLink href="/products">Products</BreadcrumbLink>
+            <BreadcrumbLink href="/products">
+              {breadcrumbTranslations.products}
+            </BreadcrumbLink>
           </BreadcrumbItem>
           <BreadcrumbSeparator />
           <BreadcrumbItem>
-            <BreadcrumbPage>Add</BreadcrumbPage>
+            <BreadcrumbPage>{breadcrumbTranslations.add}</BreadcrumbPage>
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
 
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 px-4">
-          <div className="grid grid-cols-2 gap-2">
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Price</FormLabel>
-                  <FormControl>
-                    <Input placeholder="price" type="number" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div className="grid grid-cols-2 gap-2">
-            <FormField
-              control={form.control}
-              name="category"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Category</FormLabel>
-                  <FormControl>
-                    <Select
-                      onValueChange={field.onChange}
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger className="w-full">
-                          <SelectValue
-                            placeholder={
-                              isPending ? "Loading..." : "Select category"
-                            }
-                            disabled={isPending}
-                          />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectGroup>
-                          {categories?.map((category) => (
-                            <SelectItem key={category._id} value={category._id}>
-                              {category.name}
-                            </SelectItem>
-                          ))}
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="minStock"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Min Stock</FormLabel>
-                  <FormControl>
-                    <Input placeholder="min stock" type="number" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <FormField
-            control={form.control}
-            name="description"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Description</FormLabel>
-                <FormControl>
-                  <Textarea {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="image"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Image</FormLabel>
-                <FormControl>
-                  <Input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleFileChange}
-                  />
-                </FormControl>
-                <FormMessage />
-                {previewImg && (
-                  <div className="relative size-36 rounded-sm overflow-hidden">
-                    <Image
-                      src={previewImg}
-                      fill
-                      alt="Preview"
-                      className="object-cover"
-                    />
-                  </div>
-                )}
-              </FormItem>
-            )}
-          />
-
-          <Button disabled={saving} type="submit" className="mr-4">
-            {saving ? <Loader2Icon className="animate-spin" /> : "Save"}
-          </Button>
-        </form>
-      </Form>
+      <FormAdd translations={formTranslations} />
     </div>
   )
 }
